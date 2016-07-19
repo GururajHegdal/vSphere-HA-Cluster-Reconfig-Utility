7b9712802c67df2e199722d88a89ca263c238bb3
Gururaj Hegdal <gururajahegdal@gmail.com>
2016-07-19 14:50:56 +0530

    Add files via upload

--- src/reconfigha/FDMConfigUpdater.java
+++ src/reconfigha/FDMConfigUpdater.java
@@ -1,3 +1,41 @@
+/**
+ * Utility class to add HA Cluster Advanced option and reconfigure HA on all clustered hosts - for changes
+ * to take effect. And restore the HA cluster configuration, incase there is a failure.
+ *
+ * -- Add advanced option 'das.heartbeatDsPerHost' with value '3' [HA chooses by default 2 heartbeat datastores for
+ *  each host in an HA cluster. This option can be used to increase the number to a value in the range of 2 to 5 inclusive.]
+ * -- Reconfigure HA on all Clustered hosts, in a MULTI THREADED fashion. Wait until all reconfigure HA tasks complete
+ * -- If there is a failure, report what all ESXi hosts reconfigure HA task failed AND
+ *    Revert the configuration changes made
+ *    ---- Remove added advanced option
+ *    ---- Reconfigure HA on all hosts, to revert the state
+ *
+ * Copyright (c) 2016
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * @author Gururaja Hegdal (ghegdal@vmware.com)
+ * @version 1.0
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package reconfigha;
 
 import java.net.URL;
@@ -21,21 +59,6 @@ import com.vmware.vim25.mo.ManagedEntity;
 import com.vmware.vim25.mo.ServiceInstance;
 import com.vmware.vim25.mo.Task;
 
-/**
- * Utility class to add HA Cluster Advanced option and reconfigure HA on all clustered hosts - for changes
- * to take effect. And restore the HA cluster configuration, incase there is a failure.
- *
- * -- Add advanced option 'das.heartbeatDsPerHost' with value '3' [HA chooses by default 2 heartbeat datastores for
- *  each host in an HA cluster. This option can be used to increase the number to a value in the range of 2 to 5 inclusive.]
- * -- Reconfigure HA on all Clustered hosts, in a MULTI THREADED fashion. Wait until all reconfigure HA tasks complete
- * -- If there is a failure, report what all ESXi hosts reconfigure HA task failed AND
- *    Revert the configuration changes made
- *    ---- Remove added advanced option
- *    ---- Reconfigure HA on all hosts, to revert the state
- *
- * @author Gururaja Hegdal (ghegdal@vmware.com)
- * @version 1.0
- */
 public class FDMConfigUpdater
 {
     private String vsphereIp;
--- src/reconfigha/RunApp.java
+++ src/reconfigha/RunApp.java
@@ -1,7 +1,5 @@
-package reconfigha;
-
 /**
- * Entry point into the HA reconfig sample
+ * Entry point into the vSphere HA Cluster reconfig sample
  * -- Add advanced option 'das.heartbeatDsPerHost' with value '3' [HA chooses by default 2 heartbeat datastores for
  *  each host in an HA cluster. This option can be used to increase the number to a value in the range of 2 to 5 inclusive.]
  * -- Reconfigure HA on all Clustered hosts, in a MULTI THREADED fashion. Wait until all reconfigure HA tasks complete
@@ -10,9 +8,34 @@ package reconfigha;
  *    ---- Remove added advanced option
  *    ---- Reconfigure HA on all hosts, to revert the state
  *
+ * Copyright (c) 2016
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
  * @author Gururaja Hegdal (ghegdal@vmware.com)
  * @version 1.0
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
+
+package reconfigha;
+
 public class RunApp
 {
     /**
